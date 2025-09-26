f57da1b416e0c2dff6960f6604727854e20dd2b9
TimeSuwichak <suwichak.thapchareon@gmail.com>
2025-09-26 21:19:14 +0700

    login

---
+++ TechJob/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPage from "./components/LoginPage";
+
+function App() {
+  return (
+    <div className="bg-black min-h-screen">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
